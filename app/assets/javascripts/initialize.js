b95095c658806270a00fc5982197bd9fa3fee91e
Kia Salehi <ksalehi@wellesley.edu>
2016-10-27 23:21:34 -0700

    carousel looking better; added nav bar skeleton

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
-    arrows: true,
+    arrows: false,
     dots: true,
     autoplay: true,
     autoplaySpeed: 2000,
