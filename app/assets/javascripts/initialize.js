0e49cc10dec3165bee2663ac0b3669e01029a3bc
Kia Salehi <ksalehi@wellesley.edu>
2016-10-27 22:27:23 -0700

    basic carousel functionality

---
+++ app/assets/javascripts/initialize.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef*/
+
+$(document).ready(() => {
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
