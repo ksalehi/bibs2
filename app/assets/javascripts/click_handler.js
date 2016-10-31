4174cf23d57d3e7b676b167105b5db5e30366b5a
Kia Salehi <ksalehi@wellesley.edu>
2016-10-31 00:30:46 -0700

    added links and started fleshing out feature box

--- app/assets/javascripts/click_handler.js
+++ app/assets/javascripts/click_handler.js
@@ -1,6 +1,13 @@
+function linkTo(domNode, link) {
+  $(domNode).click(() => {
+    window.location = link;
+  });
+}
 
 $(document).ready(()=>{
-  $('.learn-more-button').click(() => {
-    
-  });
+  linkTo('.for-brown', 'https://www.brown.edu/academics/brain-science/');
+  linkTo('.events', 'bit.ly/Mj3ibo');
+  linkTo('.donate', 'http://giving.brown.edu/priorities/brain-science');
+  linkTo('.news', 'https://www.brown.edu/academics/brain-science/news');
+  linkTo('.feature-text', 'https://vivo.brown.edu/display/gbarnea');
 });
--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -6,9 +6,8 @@ $(document).ready(() => {
     slidesToScroll: 1,
     infinite: true,
     arrows: true,
-    appendArrows: $('.carousel-subtext'),
     dots: true,
-    autoplay: false,
-    autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
   });
 });
