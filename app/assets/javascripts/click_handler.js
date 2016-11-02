3e98cf3c7fc855cec58ea39e09611824ad43e8ff
Kia Salehi <ksalehi@wellesley.edu>
2016-11-02 00:50:30 -0700

    added feature box details

--- app/assets/javascripts/click_handler.js
+++ app/assets/javascripts/click_handler.js
@@ -4,10 +4,32 @@ function linkTo(domNode, link) {
   });
 }
 
+function toggleShowing(jQueryObj) {
+  if (jQueryObj.hasClass('hidden')) {
+    jQueryObj.removeClass('hidden');
+  } else {
+    jQueryObj.addClass('hidden');
+  }
+}
+
+function toggleCaret(jQueryObj) {
+  if (jQueryObj.hasClass('fa-caret-right')) {
+    jQueryObj.removeClass('fa-caret-right');
+    jQueryObj.addClass('fa-caret-down');
+    window.scrollTo(0, 2000);
+  } else {
+    jQueryObj.removeClass('fa-caret-down');
+    jQueryObj.addClass('fa-caret-right');
+  }
+}
+
 $(document).ready(()=>{
   linkTo('.for-brown', 'https://www.brown.edu/academics/brain-science/');
   linkTo('.events', 'https://bit.ly/Mj3ibo');
   linkTo('.donate', 'http://giving.brown.edu/priorities/brain-science');
   linkTo('.news', 'https://www.brown.edu/academics/brain-science/news');
-  linkTo('.feature-text', 'https://vivo.brown.edu/display/gbarnea');
+  $('.feature-1-title').click(() => {
+    toggleShowing($('.feature-dropdown--1'));
+    toggleCaret($('.fa-caret--1'));
+  });
 });
